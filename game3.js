a1f40a8ed8c2782b0c821db73c2e751e88812a55
Kangas Joel <jokangas25@students.oamk.fi>
2026-04-19 23:33:59 +0300

    isotmuutokset

--- game3.js
+++ game3.js
@@ -1,27 +1,40 @@
-const symbols = ["X","P","S","A"];
-let cards = [...symbols, ...symbols];
+const images = [
+    "imags/kuva1.png",
+    "imags/kuva2.png",
+    "imags/kuva3.png",
+    "imags/kuva4.png"
+];
 
-cards.sort(() => 0.5 - Math.random());
+let cards = [...images, ...images] ;
+cards.sort(() => 0.5 - Math.random()) ;
 
-const board = document.getElementById("gameBoard");
+const board = document.getElementById("gameBoard") ;
 
-let first =  null;
-let second = null;
+// 🔥 pakotetaan voittoruutu piiloon alussa
+const winMessage = document.getElementById("winMessage");
+winMessage.classList.add("hidden");
+
+let first = null ;
+let second = null ;
 let lock = false;
 
 let matches = 0;
-const totalPairs = symbols.length; 
+const totalPairs = images.length;
 
-cards.forEach(symbol => { 
+cards.forEach(image => {
     const card = document.createElement("div");
-    card.classList.add("card");
-    card.dataset.symbol = symbol;
-    card.textContent = " "; 
+    card.classList.add("card") ;
+    card.dataset.image = image;
+
+    const img = document.createElement("img");
+    img.src = image ;
+
+    card.appendChild(img);
 
     card.addEventListener("click", () => {
-        if (lock || card === first) return;
+        if (lock || card === first || card.classList.contains("matched")) return;
 
-        card.textContent = symbol;
+        card.classList.add("flipped");
 
         if (!first) {
             first = card;
@@ -31,33 +44,37 @@ cards.forEach(symbol => {
         second = card;
         lock = true;
 
-        if (first.dataset.symbol === second.dataset.symbol) {
+        if (first.dataset.image === second.dataset.image) {
             matches++;
 
-            first.classList.add("matched");
-            second.classList.add("matched");
+            first.classList.add("matched") ;
+            second.classList.add("matched")  ;
 
             if (matches === totalPairs) {
-                document.getElementById("winMessage").classList.remove("hidden");
+                winMessage.classList.remove("hidden");
             }
 
             first = null;
             second = null;
             lock = false;
+
         } else {
             setTimeout(() => {
-                first.textContent = "";
-                second.textContent = "";
+                first.classList.remove("flipped");
+                second.classList.remove("flipped")
+                ;
+
                 first = null;
-                second = null;
-                lock = false;
+                second = null ;
+                lock = false ;
             }, 1000);
         }
     });
 
-    board.appendChild(card);
+    board.appendChild(card) ;
 });
 
 function restartGame() {
-    location.reload();
+    location.reload()
+    ;
 }
